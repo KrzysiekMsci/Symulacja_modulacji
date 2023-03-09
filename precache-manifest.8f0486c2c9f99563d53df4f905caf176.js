a628e1e7f821ed751b4e71e3fe142946e0608d09
krzysiekmsci <krzysiekmsci@gmail.com>
2023-03-10 00:14:59 +0100

    Updates

---
+++ precache-manifest.8f0486c2c9f99563d53df4f905caf176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf865c004439373a3ed0",
+    "url": "/Symulacja_modulacji/static/css/main.3651ed67.chunk.css"
+  },
+  {
+    "revision": "cf865c004439373a3ed0",
+    "url": "/Symulacja_modulacji/static/js/main.62c56d94.chunk.js"
+  },
+  {
+    "revision": "f01d59db39de6d1832ff",
+    "url": "/Symulacja_modulacji/static/js/runtime~main.5f4841a5.js"
+  },
+  {
+    "revision": "4fd98999a5bc95627e10",
+    "url": "/Symulacja_modulacji/static/js/2.42fd0b18.chunk.js"
+  },
+  {
+    "revision": "2415f16e36f233ef5e70cf8ece9cb541",
+    "url": "/Symulacja_modulacji/index.html"
+  }
+];
